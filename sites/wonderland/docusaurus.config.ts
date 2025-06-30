593e88d9f8801f220dadf25da20c0dcc6b75499b
th0rOdinson <144699868+th0rOdinson@users.noreply.github.com>
2025-06-30 14:11:41 -0300

    feat: monorepo common configuration

--- sites/wonderland/docusaurus.config.ts
+++ sites/wonderland/docusaurus.config.ts
@@ -1,128 +1,70 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
-import llmsTxtPlugin from './plugins/llmsTxtPlugin';
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import { merge } from "webpack-merge";
+import commonConfig from "@common-config/preset/commonDocusaurusConfig";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
-const config: Config = {
-  title: 'Wonderland Handbook',
+const localConfig: Config = {
+  title: "Wonderland Handbook",
   tagline:
-    'A curated guide to our best practices, processes, and technical insights.',
-  favicon: 'img/favicon.ico',
+    "A curated guide to our best practices, processes, and technical insights.",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://handbook.defi.sucks/',
+  url: "https://handbook.defi.sucks/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'defi-wonderland', // Usually your GitHub org/user name.
-  projectName: 'handbook', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace 'en' with 'zh-Hans'.
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  plugins: [
-    'docusaurus-lunr-search',
-    'vercel-analytics',
-    llmsTxtPlugin,
-  ],
+  organizationName: "defi-wonderland", // Usually your GitHub org/user name.
+  projectName: "handbook", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the 'edit this page' links.
-          editUrl: 'https://github.com/defi-wonderland/handbook/tree/main/',
+          editUrl: "https://github.com/defi-wonderland/handbook/tree/main/",
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
         blog: false,
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: [
+            "./static/common/styles/global.css",
+            "./src/css/local.css",
+          ],
         },
       } satisfies Preset.Options,
     ],
   ],
 
-  stylesheets: [
-    {
-      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
-      type: 'text/css',
-      integrity:
-        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
-      crossorigin: 'anonymous',
-    },
-  ],
-
   themeConfig: {
-    image: 'img/wonderland-social-card.png',
-    colorMode: {
-      defaultMode: 'dark',
-      disableSwitch: true,
-      respectPrefersColorScheme: false,
-    },
+    image: "img/wonderland-social-card.png",
     navbar: {
       logo: {
-        alt: 'Wonderland Handbook',
-        src: 'img/logo.svg',
-        style: { height: '100%', width: 'auto' },
+        alt: "Wonderland Handbook",
+        src: "img/logo.svg",
+        style: { height: "100%", width: "auto" },
       },
-      style: 'dark',
+      style: "dark",
       items: [
         {
-          to: 'https://wonderland.xyz',
-          position: 'right',
-          label: 'Wonderland.xyz',
+          to: "https://wonderland.xyz",
+          position: "right",
+          label: "Wonderland.xyz",
         },
       ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          html: '<a href="https://x.com/defi_wonderland" target="_blank" rel="noopener noreferrer" style="display: inline-flex; align-items: center; color: white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><g clip-path="url(#a)"><path fill="white" d="M14.095 10.317 22.286 1h-1.94L13.23 9.088 7.552 1H1l8.59 12.231L1 23h1.94l7.51-8.543L16.448 23H23M3.64 2.432h2.982l13.723 19.207h-2.982"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></svg></a>',
-        },
-        {
-          html: '<a href="https://github.com/defi-wonderland" target="_blank" rel="noopener noreferrer" style="display: inline-flex; align-items: center; color: white; text-decoration: none;"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><g clip-path="url(#a)"><g clip-path="url(#b)"><path fill="white" d="M12.012 2.036a9.868 9.868 0 0 0-6.488 2.437 10.295 10.295 0 0 0-3.395 6.159c-.38 2.4.087 4.861 1.318 6.941a10.048 10.048 0 0 0 5.402 4.41c.497.094.682-.226.682-.496v-1.742c-2.782.629-3.37-1.37-3.37-1.37a2.733 2.733 0 0 0-1.113-1.5c-.903-.63.074-.63.074-.63.317.045.619.164.884.347.265.183.486.425.645.709.204.38.515.689.891.888a2.091 2.091 0 0 0 2.02-.033 2.192 2.192 0 0 1 .614-1.37c-2.223-.258-4.556-1.137-4.556-5.058a4.012 4.012 0 0 1 1.025-2.745c-.3-.884-.266-1.85.098-2.709 0 0 .842-.276 2.751 1.049 1.64-.46 3.37-.46 5.01 0 1.91-1.325 2.746-1.049 2.746-1.049.368.857.405 1.825.105 2.709a4.012 4.012 0 0 1 1.025 2.745c0 3.934-2.34 4.794-4.568 5.026a2.466 2.466 0 0 1 .681 1.885v2.809c0 .334.178.591.688.496a10.05 10.05 0 0 0 5.381-4.416 10.435 10.435 0 0 0 1.308-6.93 10.302 10.302 0 0 0-3.386-6.15A9.875 9.875 0 0 0 12.013 2v.037l-.001-.001Z"/></g></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h24v24H0z"/></clipPath><clipPath id="b"><path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></svg></a>',
-        }
-      ],
-      copyright: `© ${new Date().getFullYear()}. Wonder LTD. All Rights Reserved`,
-    },
-    prism: {
-      theme: {...prismThemes.dracula},
-      darkTheme: {...prismThemes.dracula},
-      // Available languages: https://github.com/PrismJS/prism/tree/master/components
-      // Default list: languagesToBundle - https://github.com/FormidableLabs/prism-react-renderer/blob/master/packages/generate-prism-languages/index.ts#L9-L26
-      additionalLanguages: ['solidity', 'bash', 'mermaid', 'java', 'nasm'],
-    },
-    docs: {
-      sidebar: {
-        hideable: true,
-        autoCollapseCategories: true,
-      },
-    },
-    tableOfContents: {
-      minHeadingLevel: 2,
-      maxHeadingLevel: 4,
     },
   } satisfies Preset.ThemeConfig,
 };
 
-export default config;
+export default merge(commonConfig, localConfig) as Config;
