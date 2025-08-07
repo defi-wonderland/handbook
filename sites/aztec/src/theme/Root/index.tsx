e5108f4673803e884cd0ed5eb89c9aece417f0c2
Lumi (Wonderland) <lumi@wonderland.xyz>
2025-08-07 12:05:31 -0300

    fix: add aztec url to disclaimer

--- sites/aztec/src/theme/Root/index.tsx
+++ sites/aztec/src/theme/Root/index.tsx
@@ -4,7 +4,7 @@ export default function Root({ children }: { children: React.ReactNode }) {
   return (
     <SharedRoot
       showDisclaimer
-      disclaimerContent="This handbook is not intended to replace official documentation. This is internal material used for onboarding new team members. We open source it in the hopes that it helps somebody else, but beware it can be outdated on the latest updates."
+      disclaimerContent="This handbook is not intended to replace [official documentation](https://docs.aztec.network/). This is internal material used for onboarding new team members. We open source it in the hopes that it helps somebody else, but beware it can be outdated on the latest updates."
     >
       {children}
     </SharedRoot>
